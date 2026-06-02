8e6b042c01d4e14e31d24e082e30665185d5adcc
kevan2675-LNX <kevan2675@gmail.com>
2026-06-03 00:47:46 +0700

    Hyprland Flake

--- system.nix
+++ system.nix
@@ -101,10 +101,10 @@ in {
 
   programs.hyprland.enable = true;
 
-  programs.ags = {
-     enable = true;
-     configDir = ./config/home/files/ags;
-  };
+  #programs.ags = {
+   #  enable = true;
+    # configDir = ./config/home/files/ags;
+  #};
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1"; # Memaksa aplikasi Electron (Discord/VSCode) jalan di Wayland
   };
@@ -329,4 +329,4 @@ in {
   };
 
   system.stateVersion = "25.11"; # Sesuaikan dengan stateVersion configuration.nix lama kamu
-};
+}
