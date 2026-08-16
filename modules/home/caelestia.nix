dbbd689f08fff6c5391812ee6cd69a5338594eaf
kevan2675-LNX <kevan2675@gmail.com>
2026-08-16 13:20:26 +0700

    Hyprland-NixOS

--- modules/home/caelestia.nix
+++ modules/home/caelestia.nix
@@ -17,5 +17,11 @@ in {
       Restart = "on-failure";
     };
   };
+
+  gtk = {
+        enable = true;
+        iconTheme.package = lib.mkForce pkgs.papirus-icon-theme;
+        iconTheme.name = lib.mkForce "Papirus-Dark";
+  };
 }
   
